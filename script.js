0f7927f5826d6284d03b6a2422c600939c096180
Chris Di Luca <chrisdiluca@Chriss-MacBook-Pro.local>
2023-11-17 20:35:46 +0000

    lots

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Initialize Phaser
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -20,14 +19,14 @@ var config = {
 var player;
 var platforms;
 var cursors;
-var enemies;
 
 var game = new Phaser.Game(config);
 
 function preload() {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
-    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('Walk', 'assets/Walk.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('Idle', 'assets/Idle.png', { frameWidth: 32, frameHeight: 48 });
 }
 
 function create() {
@@ -39,51 +38,48 @@ function create() {
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
     // Player
-    player = this.physics.add.sprite(100, 450, 'dude');
+    player = this.physics.add.sprite(100, 450, 'Walk');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
+
+    // Adjust player body size for better collision
+    player.setSize(32, 48);
+    player.setOffset(0, 0);
+
     this.physics.add.collider(player, platforms);
 
     // Player animations
     this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        key: 'Walk',
+        frames: this.anims.generateFrameNumbers('Walk', { start: 0, end: 3 }),
         frameRate: 10,
         repeat: -1
     });
+
     this.anims.create({
-        key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
-        frameRate: 20
-    });
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        key: 'Idle',
+        frames: this.anims.generateFrameNumbers('Idle', { start: 0, end: 3 }),
         frameRate: 10,
         repeat: -1
     });
 
     // Input Events
     cursors = this.input.keyboard.createCursorKeys();
-
-    // Enemies
-    enemies = this.physics.add.group();
-    enemies.create(600, 450, 'dude').setFlipX(true);
-    this.physics.add.collider(enemies, platforms);
 }
 
 function update() {
     if (cursors.left.isDown) {
         player.setVelocityX(-160);
-        player.anims.play('left', true);
+        player.anims.play('Walk', true);
     } else if (cursors.right.isDown) {
         player.setVelocityX(160);
-        player.anims.play('right', true);
+        player.anims.play('Walk', true);
     } else {
         player.setVelocityX(0);
-        player.anims.play('turn');
+        player.anims.play('Idle', true);
     }
 
+    // Jumping logic
     if (cursors.up.isDown && player.body.touching.down) {
         player.setVelocityY(-330);
     }
